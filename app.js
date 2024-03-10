0d0dc4e244870cf477a605fc21a3a98a9c1457d8
Tomestre <gabrielveleci@gmail.com>
2024-03-10 18:15:28 -0300

    feat:linktree

---
+++ app.js
@@ -0,0 +1,40 @@
+function playSound() {
+    const mySound = document.getElementById("mySound");
+    const play = document.getElementById('play');
+    const pause = document.getElementById("pause")
+    const logoPlayer = document.getElementById('music-icon')
+    const logoPng = document.getElementById('logo_png')
+
+    const backgroundLogo = document.querySelector('.logo_player')
+
+    setTimeout(() => {
+        if (logoPlayer.style.display === 'none') {
+            logoPlayer.style.display = '';
+            logoPng.style.display = 'none';
+            backgroundLogo.style.background = '#22585f'
+            
+        } else {
+            logoPlayer.style.display = 'none';
+            logoPng.style.display = 'block';
+            backgroundLogo.style.background = ''
+            return
+        }
+    }, 5000)
+
+    if (mySound.paused) {
+        mySound.play();
+        play.classList.add('active')
+        pause.classList.remove('active')
+        return
+    } else {
+        mySound.pause();
+        play.classList.remove('active')
+        pause.classList.add('active')
+        return
+    }
+
+}
+
+playSound()
+
+
